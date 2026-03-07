952fa7d96753011f7108e94e03e17ce7f5f1517e
jpuy2022 <jpuy2022@gmail.com>
2026-03-07 20:58:05 -0300

    add ex 1

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -5,5 +5,10 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.WriteLine("What is your first name?");
+        string name = console.readline();
+        Console.WriteLine("What is your last name?");
+        string lastName = console.readline();
+        Console.WriteLine($"Your name is {lastName}, {name} {lastName}.");
     }
 }
